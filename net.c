04a526bf6d4948488fcbff35b6ef5639262d97e2
taku <taku>
2023-08-18 10:15:03 +0000

    finish step12

--- net.c
+++ net.c
@@ -9,6 +9,7 @@
 #include "net.h"
 #include "ip.h"
 #include "icmp.h"
+#include "arp.h"
 
 struct net_protocol
 {
@@ -296,6 +297,14 @@ int net_init(void)
     if (intr_init() == -1)
     {
         errorf("intr_init() failure");
+        return -1;
+    }
+    /*
+    Exercise 13-5: ARPの初期化関数を呼び出す
+    */
+    if (arp_init() == -1)
+    {
+        errorf("arp_init() failure");
         return -1;
     }
     if (ip_init() == -1)
